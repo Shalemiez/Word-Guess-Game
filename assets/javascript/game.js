5334215a2e4cfa2f3afd65dd0e572a928715220a
shalemiez <shalemie@icloud.com>
2019-10-04 11:14:05 -0700

    whys not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,4 @@
-
-
-
-function randomLetter(letter) {
+function randomLetter() {
     var letter = "abcdefghijklmnopqrstuvwxyz"
     var num = Math.floor((Math.random() * letter.length + 1));
     var ranletter = letter.charAt(num);
@@ -13,18 +10,18 @@ function randomLetter(letter) {
 //     document.querySelector('#guess').textContent = guess.key + (',');
 // };
 
-
+var win = 0;
+var loss = 0;
+var guestleft = 9;
+document.onkeyup = function guess() {
+    console.log(guess.key);
+    document.querySelector('#guess').textContent = guess.key + (',');
+};
 
 
 for (var i = 0; i < 10; i++) {
-    var win = 0;
-    var loss = 0;
-    var guestleft = 9;
-    document.onkeyup = function (guess) {
-        console.log(guess.key);
-        document.querySelector('#guess').textContent = guess.key + (',');
-    };
-    if (randomLetter(letter) === (guess)) {
+
+    if (randomLetter() === guess()) {
         console.log("you got me");
         win++
         guestleft--;
@@ -34,4 +31,3 @@ for (var i = 0; i < 10; i++) {
         guestleft--;
     }
 }
-
