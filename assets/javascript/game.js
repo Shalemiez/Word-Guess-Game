094b7e8f540486b2a4b5d65531cc3f420d5e23be
shalemiez <shalemie@icloud.com>
2019-10-03 20:34:12 -0700

    adding things together

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,37 @@
-// var win = 0;
-// var loss = 0;
-// var guestleft = 9;
-
-
-// function randomLetter(letter) {
-//     var letter = "abcdefghijklmnopqrstuvwxyz"
-//     var num = Math.floor((Math.random() * letter.length + 1));
-//     var ranletter = letter.charAt(num);
-//     return ranletter;
-
-// }
-
-// for (var i = 0; i < 10; i++) {
-//     if (letter === e) {
-//         console.log("you got me")
-//         win = this.win + 1
-//     } else {
-//         console.log("nope")
-//         loss = this.loss + 1
-//     }
-// }
-
-
-document.onkeyup = function (event) {
-    console.log(event.key);
-    document.querySelector('#guess').textcontent = event.key;
-};
 
 
 
-// function makeid(length) {
-//     var result = '';
-//     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-//     var charactersLength = characters.length;
-//     for (var i = 0; i < length; i++) {
-//         result += characters.charAt(Math.floor(Math.random() * charactersLength));
-//     }
-//     return result;
-// }
+function randomLetter(letter) {
+    var letter = "abcdefghijklmnopqrstuvwxyz"
+    var num = Math.floor((Math.random() * letter.length + 1));
+    var ranletter = letter.charAt(num);
+    return ranletter;
 
-// console.log(makeid(5));
+}
+// document.onkeyup = function (guess) {
+//     console.log(guess.key);
+//     document.querySelector('#guess').textContent = guess.key + (',');
+// };
+
+
+
+
+for (var i = 0; i < 10; i++) {
+    var win = 0;
+    var loss = 0;
+    var guestleft = 9;
+    document.onkeyup = function (guess) {
+        console.log(guess.key);
+        document.querySelector('#guess').textContent = guess.key + (',');
+    };
+    if (randomLetter(letter) === (guess)) {
+        console.log("you got me");
+        win++
+        guestleft--;
+    } else {
+        console.log("nope");
+        loss = this.loss + 1;
+        guestleft--;
+    }
+}
+
