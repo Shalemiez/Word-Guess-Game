ed84f0dd12b75ae728e819a35ef8d85dcec35d78
shalemiez <shalemie@icloud.com>
2019-10-05 23:13:22 -0700

    Almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,19 @@
 var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var computer = randomLetter();
+var win = 0;
+var loss = 0;
+var guessleft = 10;
+var letterused = [];
+
+var winDiv = document.querySelector('#win');
+winDiv.textContent = win;
+var guessDiv = document.querySelector('#guess');
+guessDiv.textContent = guess;
+var lossDiv = document.querySelector('#loss');
+lossDiv.textContent = loss;
+var guessleftDiv = document.querySelector('#guessleft');
+guessleftDiv.textContent = guessleft;
+
 //-------Creat Random Letter---------
 function randomLetter() {
     var num = Math.floor((Math.random() * letter.length));
@@ -6,58 +21,61 @@ function randomLetter() {
     console.log(ranletter)
     return ranletter;
 }
+//-----------Reset---------------
 function reset() {
-    total = 10;
-    win = 0;
+    guessleft = 10;
     loss = 0;
+    win = 0;
+    lossDiv.textContent = loss;
+    winDiv.textContent = win;
+
+    guessleftDiv.textContent = guessleft;
+
+
+
 
 }
-
-var computer = randomLetter();
-var win = 0;
-var loss = 0;
-var total = 10;
-var letterused = [];
-var winDiv = document.querySelector('#win');
-
-document.onkeyup = function (event) {
-    document.querySelector('#guess').textContent = event.key;
-    var guess = event.key;
-    console.log('b4 if ', total)
-
-    // document.querySelector('#total').textContent = total;
+//----------use input--------------
 
 
+if (guessleft !== 0) {
+    randomLetter();
+    document.onkeyup = function (event) {
+        document.querySelector('#guess').textContent = event.key;
+        var guess = event.key;
+
+        // letterused.push(guess);
+        // letterused.join(",")
+        // console.log('b4 if ', total)
+        // guessDiv.textContent = guess;
+        // count++
+        // console.log(count)
 
 
-    if (computer === guess) {
-        win++;
-        console.log('You won the game')
-        winDiv.textContent = win;
+        if (computer === guess) {
+            win++;
+            guessleft = 10;
+            alert('You got me')
+            winDiv.textContent = win;
+            guessleftDiv.textContent = guessleft;
 
 
+        } else {
+            guessleft--;
+            loss++;
+            document.querySelector('#guessleft').textContent = guessleft;
+            console.log('inside if', guessleft);
+        }
+        if (guessleft === 0) {
+            reset();
+            alert("Better Luck Next Time !")
 
-    } else {
-        loss++;
-        total--;
-        document.querySelector('#total').textContent = total;
-        console.log('inside if', total);
-        letterused.push(guess);
-    }
-    if (total === 0) {
-        alert("Game over")
-        reset();
+
+        }
     }
 
 };
 
 
-
-
-
-
-
-document.querySelector('#loss').textContent = loss;
-document.querySelector('#total').textContent = total;
 
 
