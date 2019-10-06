8be57ee83bb467f4fb1a8957b3cd655990ec597b
shalemiez <shalemie@icloud.com>
2019-10-05 21:22:58 -0700

    working on it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,15 @@
 var letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
-document.onkeyup = function (event) {
-    var guess = event.key;
-    document.querySelector('#guess').textContent = event.key;
-    console.log(event.key)
-};
+//-------Creat Random Letter---------
 function randomLetter() {
     var num = Math.floor((Math.random() * letter.length));
     var ranletter = letter[num];
+    console.log(ranletter)
     return ranletter;
+}
+function reset() {
+    total = 10;
+    win = 0;
+    loss = 0;
 
 }
 
@@ -17,39 +17,47 @@ var computer = randomLetter();
 var win = 0;
 var loss = 0;
 var total = 10;
-letterused = [];
+var letterused = [];
+var winDiv = document.querySelector('#win');
+
+document.onkeyup = function (event) {
+    document.querySelector('#guess').textContent = event.key;
+    var guess = event.key;
+    console.log('b4 if ', total)
+
+    // document.querySelector('#total').textContent = total;
 
 
 
-document.querySelector('#win').textContent = win;
+
+    if (computer === guess) {
+        win++;
+        console.log('You won the game')
+        winDiv.textContent = win;
+
+
+
+    } else {
+        loss++;
+        total--;
+        document.querySelector('#total').textContent = total;
+        console.log('inside if', total);
+        letterused.push(guess);
+    }
+    if (total === 0) {
+        alert("Game over")
+        reset();
+    }
+
+};
+
+
+
+
+
+
+
 document.querySelector('#loss').textContent = loss;
 document.querySelector('#total').textContent = total;
 
-function reset() {
-    total = 10;
-    win = 0;
-    loss = 0;
-}
 
-
-
-
-if (computer === guess) {
-    console.log("you got me");
-    win++
-
-} else {
-    console.log("nope");
-    loss++;
-    total--;
-    letterused.push(guess);
-}
-
-
-
-
-
-
-// document.getElementById('win').innerHTML = win;
-// document.getElementById('loss').innerHTML = loss;
-// document.getElementById('guessleft').innerHTML = guessleft;
